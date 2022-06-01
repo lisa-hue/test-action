527c5f9f88806315d4033e48fd45c82b21526585
lisa-hue <78603109+lisa-hue@users.noreply.github.com>
2022-06-01 23:30:53 +0800

    Update aiqicha_icp.py

--- aiqicha_icp.py
+++ aiqicha_icp.py
@@ -24,8 +24,11 @@ s =requests.session()
 s.mount('http://',HTTPAdapter(max_retries=5))
 s.mount('https://',HTTPAdapter(max_retries=5))
 def get_data(url):
-    req = s.get(url,headers=headers,timeout=15)
-    return req.json()
+    try:
+        req = s.get(url,headers=headers,timeout=15)
+        return req.json()
+    except Exception as e:
+        print(e)
     
 provinceCode_list = ["110000","120000","130000","140000","150000","210000","220000","230000","310000","320000","330000","340000","350000",
 "360000","370000","410000","420000","430000","440000","450000","460000","500000","510000","520000","530000","540000","610000","620000","630000","640000","650000"]
