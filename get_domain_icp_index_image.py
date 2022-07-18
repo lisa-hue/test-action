52445c4e91ccdedea16f20ec31c3d106b930a1b2
lisa-hue <78603109+lisa-hue@users.noreply.github.com>
2022-07-18 11:14:21 +0800

    Create get_domain_icp_index_image.py

---
+++ get_domain_icp_index_image.py
@@ -0,0 +1,68 @@
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+import difflib
+import base64
+import argparse
+import requests
+
+def get_icp_image(company_name,domain_name):
+    try:
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.get('https://icp.chinaz.com/'+domain_name)
+        icp_company_name_element=WebDriverWait(driver,50,0.5).until(EC.presence_of_element_located((By.XPATH,'//*[@id="first"]/li[1]/p/a')))
+        icp_company_name = icp_company_name_element.text
+        #公司名称模糊匹配
+        dif_result = difflib.get_close_matches(icp_company_name, [company_name])
+        #匹配成功，截图返回
+        if dif_result:
+            icp_image_base64 = driver.get_screenshot_as_base64()
+            return icp_image_base64
+            """
+            imgdata = base64.b64decode(icp_image_base64)
+            #将图片保存为文件
+            with open("temp.jpg",'wb') as f:
+                f.write(imgdata)
+            """
+    except:
+        return ""
+        
+def get_index_image(index_url):
+    try:
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.get(index_url)
+        index_image_base64 = driver.get_screenshot_as_base64()
+        return index_image_base64
+    except:
+        return ""
+
+    
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--url", dest='url', help="返回接口")
+    parser.add_argument("--uuid", dest='uuid', help="用户id")
+    parser.add_argument("--scanid", dest='scanid', help="任务id")
+    parser.add_argument("--company_name", dest='company_name', help="company_name")
+    parser.add_argument("--domain_name", dest='domain_name', help="domain_name")
+    parser.add_argument("--index_url", dest='index_url', help="index_url")
+    
+    args = parser.parse_args()
+    flag = True
+    icp_image_base64 = get_icp_image(args.company_name,args.domain_name)
+    #没获取到beian截图，GG
+    if icp_image_base64 == "":
+        index_image_base64 = ""
+        flag = False
+    else:
+        index_image_base64 = get_index_image(args.index_url)
+        if index_image_base64 == "":
+            flag = False
+        
+    info_list = {"uuid":args.uuid,"scanid":args.scanid,"icp_image_base64":icp_image_base64,"index_image_base64":index_image_base64,"flag":flag}
+    print(info_list)
+    rep = requests.post(url=args.url,json=info_list)
+    #print(rep.text)
